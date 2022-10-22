7f38b393beaceb46dfe81106e4f51ed5aa17fbdf
Aigul <gayazovaaigulgazimovna@gmail.com>
2022-10-22 17:09:09 +0300

    seminar

---
+++ Z_4/Program.cs
@@ -0,0 +1,59 @@
+﻿//Сумма элементов главной диагонали: 1+9+2 = 12
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+int Sum = SumElArray(array);
+Console.Write(Sum);
+
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+int SumElArray(int[,] inArray)
+{
+    int Sum = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            
+            if (i==j) Sum = Sum + inArray[i,j];
+        }
+         
+    }
+    return Sum;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
