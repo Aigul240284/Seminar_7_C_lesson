9de260ca3e54be05e0fdea40f5af2dc220a230cd
Aigul <gayazovaaigulgazimovna@gmail.com>
2022-10-22 17:13:56 +0300

    seminar

---
+++ Z_3/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задайте двумерный массив. Найдите элементы, 
+// у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+squaresArray(array);
+Console.WriteLine();
+PrintArray(array);
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void squaresArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i=i+1)
+    {
+               for (int j = 0; j < inArray.GetLength(1); j=j+2)
+        {
+            
+            inArray[i,j] = inArray [i,j]*inArray [i,j];
+        }
+         
+    }
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
